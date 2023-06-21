c458ac1b4d4b9e5011d11f8498b2f779ed2f97b4
Slaven Tomac <slaven.tomac@gmail.com>
2023-06-21 16:12:27 +0200

    Removed support for IE (not working with angular 16). Updated exports in public api

--- projects/angular-tree-component/src/public-api.ts
+++ projects/angular-tree-component/src/public-api.ts
@@ -3,3 +3,25 @@
  */
 
 export * from './lib/angular-tree-component.module';
+
+export { TreeModel } from './lib/models/tree.model';
+export { TreeNode } from './lib/models/tree-node.model';
+export { TreeDraggedElement } from './lib/models/tree-dragged-element.model';
+export { TreeVirtualScroll } from './lib/models/tree-virtual-scroll.model';
+export { LoadingComponent } from './lib/components/loading.component';
+export { TreeComponent } from './lib/components/tree.component';
+export { TreeNodeComponent } from './lib/components/tree-node.component';
+export { TreeNodeContent } from './lib/components/tree-node-content.component';
+export { TreeNodeDropSlot } from './lib/components/tree-node-drop-slot.component';
+export { TreeNodeExpanderComponent } from './lib/components/tree-node-expander.component';
+export { TreeNodeChildrenComponent } from './lib/components/tree-node-children.component';
+export { TreeNodeCollectionComponent } from './lib/components/tree-node-collection.component';
+export { TreeNodeWrapperComponent } from './lib/components/tree-node-wrapper.component';
+export { TreeViewportComponent } from './lib/components/tree-viewport.component';
+export { TreeNodeCheckboxComponent } from './lib/components/tree-node-checkbox.component';
+export { TreeDropDirective } from './lib/directives/tree-drop.directive';
+export { TreeDragDirective } from './lib/directives/tree-drag.directive';
+export { TreeAnimateOpenDirective } from './lib/directives/tree-animate-open.directive';
+
+export { TreeMobxAutorunDirective } from './lib/mobx-angular/tree-mobx-autorun.directive';
+ 
